3d934f1263573e5830c91dd0a702fc580b4f2c98
Flavio <flaviogonzalezdesign@gmail.com>
2026-03-06 15:37:17 -0300

    fix PJSIP identify_by directly on asterisk configs to prevent ip collision

---
+++ fix_identify.php
@@ -0,0 +1,9 @@
+<?php
+$exts = ['2001', '2002', '2003', '2004', '2005'];
+$content = "";
+foreach($exts as $i) {
+    $content .= "[$i](+)\nidentify_by=username,auth_username\n\n";
+}
+file_put_contents('/etc/asterisk/pjsip.endpoint_custom_post.conf', $content);
+echo "Custom configs applied to endpoints.\n";
+shell_exec("asterisk -rx 'pjsip reload'");
