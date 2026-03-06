f764faa6cd2a3221e638f3ab98c5881d88f89e85
Flavio <flaviogonzalezdesign@gmail.com>
2026-03-06 15:35:19 -0300

    fix PJSIP identify_by username only

--- fix_wss.php
+++ fix_wss.php
@@ -1,19 +1,18 @@
 <?php
 try {
     $db = new PDO("mysql:host=localhost;dbname=asterisk", "asteriskuser", "Sildan.1329");
+    
     // Setting SIP Channel Driver to PJSIP so WebSockets on chan_sip is disabled
     $stmt = $db->prepare("UPDATE freepbx_settings SET value='PJSIP' WHERE keyword='SIPCHANNELHW'");
     $stmt->execute();
-    echo "SIP channel hardware set to PJSIP. \n";
     
-    // Also disable websocket in advanced settings if there is any explicit flag
-    $stmt2 = $db->prepare("UPDATE freepbx_settings SET value='0' WHERE keyword LIKE '%WEB%' AND keyword LIKE '%SIP%'");
-    $stmt2->execute();
+    // Avoid PJSIP incorrectly identifying softphones behind NAT sharing same IP
+    $stmt3 = $db->prepare("UPDATE sip SET data='username,auth_username' WHERE keyword='identify_by'");
+    $stmt3->execute();
+    echo "Identified_By fix applied to " . $stmt3->rowCount() . " extensions.\n";
     
     echo "Reloading FreePBX config...\n";
     shell_exec("/var/lib/asterisk/bin/retrieve_conf");
-    shell_exec("asterisk -rx 'module unload chan_sip.so'");
-    shell_exec("asterisk -rx 'module load res_pjsip_transport_websocket.so'");
     shell_exec("asterisk -rx 'core reload'");
     echo "Done.\n";
 } catch (Exception $e) {
